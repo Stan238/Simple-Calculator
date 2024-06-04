75a4951ddf934a67d67e1fce05b2f74c8bd4654d
Stan238 <127895826+Stan238@users.noreply.github.com>
2024-06-04 10:47:24 -0400

    Initial Commit

---
+++ Simple Calculator/Simple_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_CalculatorApp.swift
+//  Simple Calculator
+//
+//  Created by Stan U on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Simple_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
